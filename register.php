7dbefc13f9adf9fd2eb395aa6b1e1b2c0b8f7775
clayhackergroup <desksecuraty@gmail.com>
2025-02-01 11:34:38 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $filename = 'w/admin/database/data.txt';
+    $filename = 'data.txt';
     $data = file_exists($filename) ? file_get_contents($filename) : '';
 
     $firstname = trim($_POST['firstname']);
